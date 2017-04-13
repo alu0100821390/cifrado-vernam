2d29af40adbfc7e696352f443766c4d144ff118e
alu0100821390 <alu0100821390@ull.edu.es>
2017-04-13 17:03:31 +0100

    Corregir fecha

--- Modification/vernam_mod.py
+++ Modification/vernam_mod.py
@@ -3,7 +3,7 @@
 ## Escuela Superior de Ingeniería y Tecnología	 				 	##
 ## Grado en Ingeniería Informática				 		 	##
 ## Seguridad en Sistemas Informáticos			 			 	##
-## Fecha: 14/02/2016							  	 	##
+## Fecha: 14/02/2017							  	 	##
 ## Autor: Kevin Estévez Expósito (alu0100821390) 				 	##
 ## 										 	##
 ## Práctica 1: Cifrado de Vernam (CON MODIFICACIÓN)				 	##
